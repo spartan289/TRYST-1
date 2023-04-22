cbfb3ae9dc824e6e5e1fd3b0320dc54b1f428ed9
shivangii123 <130161239+shivangii123@users.noreply.github.com>
2023-04-22 11:33:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -157,12 +157,12 @@
             </div>
           </div>
         </div>
-        <div class="lg:w-1/3 sm:w-1/2 p-14">
+        <div class="lg:w-1/3 sm:w-1/3 p-14">
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full  object-cover object-center"
               src="images\event\advaiitaa .png">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">ADVAITAA</h1>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">ADVAaaTAA</h1>
               <p class="leading-relaxed"> celebrates the art of dance in various western styles such as hip hop,
                     jazz, or contemporary.Get set to unleash your inner talent in tyrst'23.</p>
             </div>
