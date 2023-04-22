4bfeb37f80c85250c01cd51c18cc975efe04ce28
spartan289 <60476934+spartan289@users.noreply.github.com>
2023-04-22 15:14:32 +0530

    Update jsondata.php

--- api/jsondata.php
+++ api/jsondata.php
@@ -6,7 +6,7 @@
         mysqli_ssl_set($conn,NULL,NULL, "../".$env["MYSQL_ATTR_SSL_CA"], NULL, NULL);
     }
     mysqli_real_connect($conn, $env["AZURE_MYSQL_HOST"], $env["AZURE_MYSQL_USERNAME"], $env["AZURE_MYSQL_PASSWORD"], $env["AZURE_MYSQL_DBNAME"], 3306, MYSQLI_CLIENT_SSL);
-    $query = "select * FROM `tryst_info`";
+    $query = "select * FROM `tryst_info` where tickverif=0";
     $result = mysqli_query($conn, $query) ;
     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
