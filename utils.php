92d44362e100b4bd083f2d80ddf7cb5956d730fe
spartan289 <60476934+spartan289@users.noreply.github.com>
2023-04-23 20:32:30 +0530

    Update utils.php

--- utils.php
+++ utils.php
@@ -99,9 +99,12 @@
             $env = parse_ini_file('.env');
 
             $conn = mysqli_init();
+          
             if($env["MYSQL_ATTR_SSL_CA"] != NULL){
-                mysqli_ssl_set($conn,NULL,NULL, "../".$env["MYSQL_ATTR_SSL_CA"], NULL, NULL);
+                mysqli_ssl_set($conn,NULL,NULL, $env["MYSQL_ATTR_SSL_CA"], NULL, NULL);
+        
             }
+        
             mysqli_real_connect($conn, $env["AZURE_MYSQL_HOST"], $env["AZURE_MYSQL_USERNAME"], $env["AZURE_MYSQL_PASSWORD"], $env["AZURE_MYSQL_DBNAME"], 3306, MYSQLI_CLIENT_SSL);
 
             $query = "UPDATE `tryst_info` SET tickverif=1 WHERE c_mailId = '$email' and is_verified=1;" ;
